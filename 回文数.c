968e6eda0025b3cbc6f34364e0c3e14aa73f5c22
aaaRnb <1545178799@qq.com>
2024-05-10 09:21:15 +0800

    Add files via upload

---
+++ 回文数.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int a[10000]={0},n,b,c,d,e,i,j;
+	scanf("%d",&n);
+	e=0;
+	for(i=0;i<n;i++){
+		b=i,c=0,d=0;
+		for(j=0;b!=0;j++){
+			a[j]=b%10;
+			b=b/10;
+			d++;
+		}
+		for(j=0;d!=0;j++){
+			c=c+a[j]*pow(10,d-1);
+			d--;
+		}
+		if(c==i){
+			printf("%d,",i);
+			e++;
+			if(e%10==0)
+			printf("\n");
+		}
+	}
+}
+
+
